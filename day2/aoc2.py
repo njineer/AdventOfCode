c0884671c7a0853a11b2f93802ace97af4f66da4
Zane <zane.franklin@gtri.gatech.edu>
2018-01-11 18:04:17 -0500

    day2.2

--- day2/aoc2.py
+++ day2/aoc2.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def aoc2(filename):
+def aoc2 (filename):
     sum = 0
     with open(filename, 'r') as f:
         for line in f:
@@ -13,6 +13,23 @@ def aoc2(filename):
     return sum
 
 
+def aoc2_2 (filename):
+    sum = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.strip():
+                values = [ int(x) for x in line.strip().split() ]
+                for index, x in enumerate(values[:-1]):
+                    for y in values[index+1:]:
+                        greater = max(x,y)
+                        lesser = min(x,y)
+                        if greater % lesser == 0:
+                            sum += greater // lesser
+                            break
+                    else:
+                        continue # inner loop not broken
+                    break # break outer loop if inner loop is broken
+    return sum
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -21,3 +38,4 @@ if __name__ == '__main__':
         print("Too many arugments.", file=sys.stderr)
     else:
         print(aoc2(sys.argv[1]))
+        print(aoc2_2(sys.argv[1]))
