9261b8100af3808baa0fa150e182c9105ac5777f
Zane <zane.franklin@gtri.gatech.edu>
2018-01-11 16:22:17 -0500

    day1.2

--- day1/aoc1.cpp
+++ day1/aoc1.cpp
@@ -3,15 +3,7 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    string input;
-
-    switch (argc) {
-        case 1: getline(cin, input); break;
-        case 2: input = argv[1]; break;
-        default: cerr << "Too many arguments." << endl; return 1;
-    }
-
+int aoc1 (const string& input) {
     int sum = 0;
     auto itr1 = input.begin();
     auto itr2 = next(itr1, 1);
@@ -24,7 +16,37 @@ int main(int argc, char** argv) {
     if (input.front() == input.back())
         sum += input.back() - '0';
 
-    cout << "sum: " << sum << endl;
+    return sum;
+}
+
+int aoc1_2 (const string& input) {
+    int sum = 0;
+    int inputLength = input.size();
+    int step = inputLength/2;
+    for (int i=0; i < inputLength; ++i) {
+        int halfway = (i + step) % inputLength;
+        if (input[i] == input[halfway]) {
+            sum += input[i] - '0';
+        }
+    }
+    return sum;
+}
+
+
+int main(int argc, char** argv) {
+    string input;
+
+    switch (argc) {
+        case 1: getline(cin, input); break;
+        case 2: input = argv[1]; break;
+        default: cerr << "Too many arguments." << endl; return 1;
+    }
+
+    auto sum = aoc1(input);
+    cout << "sum 1: " << sum << endl;
+
+    auto sum2 = aoc1_2(input);
+    cout << "sum 2: " << sum2 << endl;
 
     return 0;
 }
