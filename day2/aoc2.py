a9300fb59ec4f1b20ea7ffb1b808642567be7a78
Zane <zane.franklin@gtri.gatech.edu>
2018-01-11 18:05:54 -0500

    day2.2: clarity

--- day2/aoc2.py
+++ day2/aoc2.py
@@ -20,15 +20,15 @@ def aoc2_2 (filename):
             if line.strip():
                 values = [ int(x) for x in line.strip().split() ]
                 for index, x in enumerate(values[:-1]):
+                    lineDone = False
                     for y in values[index+1:]:
                         greater = max(x,y)
                         lesser = min(x,y)
                         if greater % lesser == 0:
                             sum += greater // lesser
-                            break
-                    else:
-                        continue # inner loop not broken
-                    break # break outer loop if inner loop is broken
+                            lineDone = True
+                    if lineDone:
+                        break
     return sum
 
 if __name__ == '__main__':
