103f466bbf11421f3c9cb01b099a03d2dac7e3e9
zane <njineer@outlook.com>
2018-01-20 22:22:12 -0500

    day5.2

--- day5/day5.go
+++ day5/day5.go
@@ -8,6 +8,34 @@ import (
     "strconv"
 )
 
+func jumpsUntilExit(jumps []int) int {
+    steps := 0
+    for offset := 0; offset >= 0 && offset < len(jumps); {
+        newOffset := offset + jumps[offset]
+        jumps[offset]++
+        offset = newOffset
+        steps++
+    }
+    return steps
+}
+
+func jumpsUntilExit2(jumps []int) int {
+    steps := 0
+    for offset := 0; offset >= 0 && offset < len(jumps); {
+        //fmt.Printf("Step %v: jumps[%v] = %v -> ", steps, offset, jumps[offset])
+        newOffset := offset + jumps[offset]
+        if jumps[offset] >= 3 {
+            jumps[offset]--
+        } else {
+            jumps[offset]++
+        }
+        //fmt.Printf("jumps[%v] = %v; next = %v\n", offset, jumps[offset], newOffset)
+        offset = newOffset
+        steps++
+    }
+    return steps
+}
+
 func main() {
     var scanner* bufio.Scanner
     if len(os.Args[1:]) >= 1 {
@@ -22,19 +50,15 @@ func main() {
     jumps := make([]int, 0)
     for scanner.Scan() {
         if jump, err := strconv.Atoi(strings.TrimSpace(scanner.Text())); err != nil {
-            fmt.Println("Invalid integer: %v", scanner.Text())
+            //fmt.Println("Invalid integer: %v", scanner.Text())
             return
         } else {
             jumps = append(jumps, jump)
         }
     }
-    fmt.Println(len(jumps), " jumps")
 
-    steps := 0
-    for offset := 0; offset >= 0 && offset < len(jumps); {
-        offset, jumps[offset] = offset + jumps[offset], jumps[offset] + 1
-        steps++
-        //fmt.Printf("Step %v: jumps[%v] = %v\n", steps, offset, jumps[offset]-1)
-    }
-    fmt.Printf("%v jumps\n", steps)
+    jumps2 := make([]int, len(jumps))
+    copy(jumps2, jumps)
+    fmt.Printf("%v jumps\n", jumpsUntilExit(jumps))
+    fmt.Printf("%v jumps\n", jumpsUntilExit2(jumps2))
 }
