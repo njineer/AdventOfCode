17f64c91b67b8c6b57b18ad98a9ad81d9e1ea828
zane <njineer@outlook.com>
2018-02-05 22:17:53 -0500

    day6

--- day6/aoc6.go
+++ day6/aoc6.go
@@ -8,6 +8,44 @@ import (
     "strconv"
 )
 
+func printMemory(s *[16]int) {
+    fmt.Print("[")
+    for _, x := range s[:len(s)-1] {
+        fmt.Printf("%v,", x)
+    }
+    fmt.Printf("%v]\n", s[len(s)-1])
+}
+
+func findMaxIndex(a *[16]int) int {
+    var maxIndex, maxValue = 0, 0
+
+    for i, v := range a {
+        if v > maxValue {
+            maxValue = v
+            maxIndex = i
+        }
+    }
+    fmt.Printf("Max = %v @ %v\n", maxValue, maxIndex)
+    return maxIndex
+}
+
+func reallocate(memory *[16]int) {
+    fmt.Print("Before reallocation: ")
+    printMemory(memory)
+    // find index of max number of blocks
+    maxIndex := findMaxIndex(memory)
+    // rellocate those blocks
+    alloc := memory[maxIndex]
+    // reset previously-max bank
+    memory[maxIndex] = 0
+
+    for i := maxIndex+1; alloc > 0; i, alloc = i+1, alloc-1 {
+        memory[i%16]++
+    }
+    fmt.Print("After reallocation:  ")
+    printMemory(memory)
+}
+
 func main() {
 
     var scanner *bufio.Scanner
@@ -29,7 +67,10 @@ func main() {
 
     // memory: fixed number of banks consisting of arbitrary number of blocks
     const numBanks = 16
-    memory := make([]int, numBanks)
+    var memory [numBanks]int
+    
+    // history of bank configurations seen
+    history := make(map[[16]int]struct{})
 
     // load initial memory allocation
     scanner.Scan()
@@ -42,6 +83,17 @@ func main() {
         }
     }
 
+    reallocations := 0
+    watchdog := 1000
+    for _, seen := history[memory]; !seen && watchdog > 0; {
+        fmt.Printf("Reallocation %v: ", reallocations)
+        printMemory(&memory)
+        history[memory] = struct{}{}
+        reallocate(&memory)
+        reallocations++
+        watchdog--
+    }
 
     fmt.Println("Advent of Code: Day 6")
+    fmt.Printf("reallocations: %v\n", reallocations)
 }
