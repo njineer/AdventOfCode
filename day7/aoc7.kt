dbe59f447733041e554a600f635c3463db729dea
Zane <zane.franklin@gtri.gatech.edu>
2018-02-09 17:44:45 -0500

    day7: read input; java has insane double backslashing in regex

--- day7/aoc7.kt
+++ day7/aoc7.kt
@@ -1,7 +1,22 @@
+import java.io.File
+import kotlin.text.Regex
+
+fun addToMap(str: String): Unit {
+    println("Adding $str")
+}
+
+fun backslashify(str: String): String = str.replace("\\", "\\\\")
+
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         val filename = args[0]
-        System.out.println(filename)
+        val linePattern = Regex(backslashify("(\w+)\s+\(\d+)\)\s*(->\s+((\w+),?\s*)+)?"))
+        File(filename).forEachLine { line ->
+            if (line matches linePattern) {
+                print("Match")
+            }
+        }
+        println(bs.replace("\\", "\\\\"))
     } else {
         System.err.println("Missing input file")
     }
