74e766e9890963465ecb15e7ef84bc19880ac20a
Zane <zane.franklin@gtri.gatech.edu>
2018-02-09 17:48:44 -0500

    day7: double backslashes

--- day7/aoc7.kt
+++ day7/aoc7.kt
@@ -5,18 +5,15 @@ fun addToMap(str: String): Unit {
     println("Adding $str")
 }
 
-fun backslashify(str: String): String = str.replace("\\", "\\\\")
-
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         val filename = args[0]
-        val linePattern = Regex(backslashify("(\w+)\s+\(\d+)\)\s*(->\s+((\w+),?\s*)+)?"))
+        val linePattern = Regex("(\\w+)\\s+\\(\\d+)\\)\\s*(->\\s+((\\w+),?\\s*)+)?")
         File(filename).forEachLine { line ->
             if (line matches linePattern) {
                 print("Match")
             }
         }
-        println(bs.replace("\\", "\\\\"))
     } else {
         System.err.println("Missing input file")
     }
