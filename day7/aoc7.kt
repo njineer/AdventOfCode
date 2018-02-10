fb49475e1e9c21f8198b7281890ea4fc62be0b4f
zane <njineer@outlook.com>
2018-02-10 18:38:56 -0500

    day7: input

--- day7/aoc7.kt
+++ day7/aoc7.kt
@@ -5,17 +5,30 @@ fun addToMap(str: String): Unit {
     println("Adding $str")
 }
 
+fun <R: Any> String?.whenNotNullNorBlank(block: (String) -> R): R? {
+    return this?.let { r ->
+        if (r.isNotBlank()) {
+            block(r)
+        } else null
+    }
+}
+
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         val filename = args[0]
-        val linePattern = Regex("(\\w+)\\s+\\(\\d+)\\)\\s*(->\\s+((\\w+),?\\s*)+)?")
+        val linePattern = Regex("(\\w+)\\s+\\((\\d+)\\)\\s*(->\\s+((\\w+),?\\s*)+)?")
         File(filename).forEachLine { line ->
             if (line matches linePattern) {
-                print("Match")
+                println("Match")
             }
         }
     } else {
-        System.err.println("Missing input file")
+        while(true) {
+            readLine().whenNotNullNorBlank {
+
+            } 
+            ?: break
+        }
     }
 }
 
