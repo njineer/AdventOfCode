5b1dfcec165d96f1f8fe26181289684a3fdd7ceb
zane <njineer@outlook.com>
2018-02-22 23:41:20 -0500

    day8: in progress

--- day8/aoc8.kt
+++ day8/aoc8.kt
@@ -1,17 +1,10 @@
+package aoc8
+
 import java.io.File
 import kotlin.text.Regex
 import java.lang.Exception
 
 class InputException(override var message:String): Exception(message)
-//data class Register(val name: String, val value: Int)
-//data class RegComponents(
-//    val name: String, 
-//    val incDec: String, 
-//    val amount: Int, 
-//    val compareReg: String, 
-//    val compareFunc: String, 
-//    val compareVal: Int
-//)
 
 infix fun Int.eq(other: Int): Boolean = this.compareTo(other) == 0
 infix fun Int.ne(other: Int): Boolean = this.compareTo(other) != 0
@@ -20,13 +13,15 @@ infix fun Int.lt(other: Int): Boolean = this.compareTo(other) < 0
 infix fun Int.gt(other: Int): Boolean = this.compareTo(other) > 0
 infix fun Int.ge(other: Int): Boolean = this.compareTo(other) >= 0
 
-val comparators = mapOf(
+val operators = mapOf(
     "==" to Int::eq,
     "!=" to Int::ne,
     "<"  to Int::lt,
     "<=" to Int::le,
     ">"  to Int::gt,
-    ">=" to Int::ge
+    ">=" to Int::ge,
+    "inc" to Int::inc,
+    "dec" to Int::dec
 )
 
 
@@ -78,9 +73,29 @@ fun parseInputs(inputs: List<String>): List<List<String>> {
     return instructions.toList()
 }
 
+fun processInstructions(instructions: List<List<String>>): Map<String, Int> {
+    var registers = mutableMapOf<String, Int>()
+    instructions.forEach { instr ->
+        val reg = instr[0]
+        val op = operators.getOrElse(instr[1].toLowerCase()) { throw InputException("Expected 'inc' or 'dec'; found ${instr[1]}") }
+        val amount = instr[2].toInt()
+        val compareReg = instr[3]
+        val compareOp = operators.getOrElse(instr[4].toLowerCase()) { throw InputException("Expected a comparator; found ${instr[4]}") }
+        val compareVal = instr[5].toInt()
+
+        println("reg: $reg\nop: $op\namount: $amount\ncompareReg:$compareReg\ncompareOp: $compareOp\ncompareVal: $compareVal")
+        
+        if (registers.getOrDefault(compareReg, 0) compareOp compareVal) {
+            registers.getOrDefault(reg, 0).let { 
+                registers[reg] = registers.getOrDefault(reg, 0) op amount
+            }
+        }
+    }
+    return registers.toMap()
+}
 
 
 fun main(args: Array<String>) {
-    parseInputs(getInput(args))
+    val registers = processInstructions(parseInputs(getInput(args)))
 }
 
