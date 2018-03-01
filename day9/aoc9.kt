5d266e3a8136cbc55013a1f68349ead6a8725d41
zane <njineer@outlook.com>
2018-02-28 22:53:19 -0500

    day 9 done

---
+++ day9/aoc9.kt
@@ -0,0 +1,71 @@
+package aoc9
+
+import java.io.File
+
+fun <R> String?.whenNotNullNorBlank(block: (String) -> R): R? {
+    return this?.let { receiver ->
+        if (receiver.isNotBlank()) {
+            block(receiver)
+        } else null
+    }
+}
+
+
+fun scoreCharStream(charStream: String): Int {
+    val groupStart = '{'
+    val groupEnd = '}'
+    val garbageStart = '<'
+    val garbageEnd = '>'
+    val cancellation = '!'
+
+    var inGarbage = false
+    var cancel = false
+    var group = 0
+    var score = 0
+
+    //println(charStream)
+
+    charStream.forEach { chr ->
+        if (!cancel) {
+            when (chr) {
+                groupStart -> {
+                    if (!inGarbage) 
+                        group++
+                }
+                groupEnd -> 
+                    if (!inGarbage) 
+                        score += group--
+                garbageStart -> 
+                    inGarbage = true
+                garbageEnd ->
+                    inGarbage = false
+                cancellation ->
+                    cancel = true
+            }
+        } else {
+            cancel = false
+        }
+        //println("chr($chr), inGarbage($inGarbage), group($group), cancel($cancel), score($score)")
+    }
+
+    return score
+}
+
+fun main(args: Array<String>) {
+    var charStream = ""
+    if (args.isNotEmpty()) {
+        File(args[0]).forEachLine {
+            charStream = charStream + it
+        }
+    } else {
+        while(true) {
+            readLine().whenNotNullNorBlank {
+                charStream = charStream + it
+            }
+            ?: break
+        }
+    }
+
+    val score = scoreCharStream(charStream)
+    println(score)
+}
---
+++ day9/aoc9.sh
@@ -0,0 +1,5 @@
+if [ -f aoc9.jar ]; then
+    java -jar aoc9.jar $@
+else
+    echo "Missing .jar"
+fi
