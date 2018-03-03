bbfb1d44e66196d56f4b0df5760bad89027afc3a
zane <njineer@outlook.com>
2018-03-02 22:13:10 -0500

    fix kotlin start script

--- start_kotlin_day.sh
+++ start_kotlin_day.sh
@@ -6,6 +6,6 @@ else
     cd day$1
     touch aoc$1.kt
     echo "sources = aoc$1.kt\n\naoc$1.jar: \$(sources)\n\t@kotlinc \$^ -include-runtime -d \$@\n" > Makefile
-    echo "if [ -f aoc$1.jar ]; then\n\tjava -jar aoc$1.jar \$@\nelse\n\techo \"Missing .jar\"\nfi\n"
+    echo "if [ -f aoc$1.jar ]; then\n\tjava -jar aoc$1.jar \$@\nelse\n\techo \"Missing .jar\"\nfi\n" > aoc$1.sh
     cd -
 fi
