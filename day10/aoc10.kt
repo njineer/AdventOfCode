8e4480b1767f93c3070eb959ab919df8d242696b
zane <njineer@outlook.com>
2018-03-03 20:44:55 -0500

    day10 done

--- day10/aoc10.kt
+++ day10/aoc10.kt
@@ -10,16 +10,42 @@ fun <R> String?.whenNotNullNorBlank(block: (String) -> R): R? {
 
 fun main(args: Array<String>) {
     readLine().whenNotNullNorBlank { input ->
-        val listSize = 256
-        var list = (0..255).toList()
+        val listSize = 
+            if (args.isNotEmpty()) {
+                args[0].toInt()
+            } else {
+                256
+            }
+        var list = (0..(listSize-1)).toMutableList()
         var skipSize = 0
         var current = 0
         input
             .split(",")
             .map { it.toInt() }
             .forEach { length ->
-                
+                val overlap = current + length - listSize
+                val rangeEnd = minOf(current + length - 1, listSize - 1)
+                println("current ($current) length ($length), overlap ($overlap), rangeEnd ($rangeEnd)")
+
+                var toReverse = list.slice(current..rangeEnd)
+                if (overlap > 0) {
+                    toReverse += list.slice(0..overlap-1)
+                }
+
+                toReverse.asReversed().let { reversed ->
+                    (current..rangeEnd).forEach { i ->
+                        list[i] = reversed[i-current]
+                    }
+                    if (overlap > 0) {
+                        (0..overlap-1).forEach { i ->
+                            list[i] = reversed[length-overlap+i]
+                        }
+                    }
+                }
+                current = (current + length + skipSize) % listSize
+                skipSize++
             }
+        println(list[0]*list[1])
     }
 }
 
