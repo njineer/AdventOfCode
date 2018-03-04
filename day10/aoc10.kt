feaad54ddfefd56d5ebe21f8e77655d9b275dbb0
zane <njineer@outlook.com>
2018-03-03 23:21:03 -0500

    day10: ascii-fy input

--- day10/aoc10.kt
+++ day10/aoc10.kt
@@ -1,5 +1,9 @@
 package aoc9
 
+import java.lang.Exception
+
+class AsciiException(override var message:String): Exception(message)
+
 fun <R> String?.whenNotNullNorBlank(block: (String) -> R): R? {
     return this?.let { receiver ->
         if (receiver.isNotBlank()) {
@@ -8,8 +12,27 @@ fun <R> String?.whenNotNullNorBlank(block: (String) -> R): R? {
     }
 }
 
+// Convert a string to a list of ASCII values
+// Doesn't need to be a class, but hey: language features!
+class Asciifier(val bytes: String) {
+    companion object AsciiTable {
+        val asciiTable = mapOf (
+            ',' to 44,
+            '0' to 48, '1' to 49,
+            '2' to 50, '3' to 51,
+            '4' to 52, '5' to 53,
+            '6' to 54, '7' to 55,
+            '8' to 56, '9' to 57
+        )
+        fun bytesToList(bytes: String): List<Int> {
+            return bytes.map { asciiTable.getOrElse(it) { throw AsciiException("$it not in Ascii table") } }
+        }
+    }
+
+}
+
 class Hash(val listSize: Int, var current: Int, var skipSize: Int) {
-    val list: MutableList<Int>
+    var list: MutableList<Int>
     constructor (listSize: Int): this(listSize, 0, 0)
     init {
         list = (0..(listSize-1)).toMutableList()
@@ -45,14 +68,38 @@ class Hash(val listSize: Int, var current: Int, var skipSize: Int) {
             skipSize++
         }
     }
+
+    fun reset() {
+        list = (0..(listSize-1)).toMutableList()
+        current = 0
+        skipSize = 0
+    }
 }
 
 fun main(args: Array<String>) {
     readLine().whenNotNullNorBlank { input ->
-        val lengths = input.split(",") .map { it.toInt() }
-        val hash = Hash(256)
-        hash.singleRound(lengths)
+        val listSize = 
+            if (args.isNotEmpty()) {
+                args[0].toInt()
+            } else {
+                256
+            }
+            
+        // part 1
+        val lengths1 = input.split(",") .map { it.toInt() }
+        val hash = Hash(listSize)
+        hash.singleRound(lengths1)
         println(hash.list[0]*hash.list[1])
+
+        hash.reset()
+
+        val suffix = listOf(17, 31, 73, 47, 23)
+        val lengths2 = Asciifier.bytesToList(input) + suffix
+        println(lengths2)
+        
+        // part 2
+
+
     }
 }
 
