b6b0021437adaffbcbcef75a53f704b41d64a6ee
zane <njineer@outlook.com>
2018-03-03 22:31:21 -0500

    day10: rework for part 2

--- day10/aoc10.kt
+++ day10/aoc10.kt
@@ -8,49 +8,51 @@ fun <R> String?.whenNotNullNorBlank(block: (String) -> R): R? {
     }
 }
 
+class Hash(val listSize: Int, var current: Int, var skipSize: Int) {
+    val list: MutableList<Int>
+    constructor (listSize: Int): this(listSize, 0, 0)
+    init {
+        list = (0..(listSize-1)).toMutableList()
+    }
+
+    fun singleRound(lengths: List<Int>) {
+        // default to list size of 256 if not specified
+        lengths.forEach { length ->
+            // amount we wrap around if we do
+            val wrapAround = current + length - listSize
+            // end of the non-wrap-around section to reverse
+            val rangeEnd = minOf(current + length - 1, listSize - 1)
+    
+            // section to be reversed
+            var toReverse = list.slice(current..rangeEnd)
+            // add the rest of the section if we wrapped around
+            if (wrapAround > 0) {
+                toReverse += list.slice(0..wrapAround-1)
+            }
+    
+            // reverse the section and overwrite the corresponding items in the original
+            toReverse.asReversed().let { reversed ->
+                (current..rangeEnd).forEach { i ->
+                    list[i] = reversed[i-current]
+                }
+                if (wrapAround > 0) {
+                    (0..wrapAround-1).forEach { i ->
+                        list[i] = reversed[length-wrapAround+i]
+                    }
+                }
+            }
+            current = (current + length + skipSize) % listSize
+            skipSize++
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     readLine().whenNotNullNorBlank { input ->
-        // default to list size of 256 if not specified
-        val listSize = 
-            if (args.isNotEmpty()) {
-                args[0].toInt()
-            } else {
-                256
-            }
-        var list = (0..(listSize-1)).toMutableList()
-        var skipSize = 0
-        var current = 0
-        input
-            .split(",")
-            .map { it.toInt() }
-            .forEach { length ->
-                // amount we wrap around if we do
-                val wrapAround = current + length - listSize
-                // end of the non-wrap-around section to reverse
-                val rangeEnd = minOf(current + length - 1, listSize - 1)
-
-                // section to be reversed
-                var toReverse = list.slice(current..rangeEnd)
-                // add the rest of the section if we wrapped around
-                if (wrapAround > 0) {
-                    toReverse += list.slice(0..wrapAround-1)
-                }
-
-                // reverse the section and overwrite the corresponding items in the original
-                toReverse.asReversed().let { reversed ->
-                    (current..rangeEnd).forEach { i ->
-                        list[i] = reversed[i-current]
-                    }
-                    if (wrapAround > 0) {
-                        (0..wrapAround-1).forEach { i ->
-                            list[i] = reversed[length-wrapAround+i]
-                        }
-                    }
-                }
-                current = (current + length + skipSize) % listSize
-                skipSize++
-            }
-        println(list[0]*list[1])
+        val lengths = input.split(",") .map { it.toInt() }
+        val hash = Hash(256)
+        hash.singleRound(lengths)
+        println(hash.list[0]*hash.list[1])
     }
 }
 
