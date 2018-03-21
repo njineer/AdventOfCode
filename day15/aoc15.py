98ae36b3c5c07930bd11d9ce8b87d1c10aeb3488
Zane <zane.franklin@gtri.gatech.edu>
2018-03-21 10:45:16 -0400

    day15: part 1&2 done

--- day15/aoc15.py
+++ day15/aoc15.py
@@ -1,37 +1,57 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 import sys
+import argparse
+from itertools import islice
+from collections import namedtuple
 
+GenParam = namedtuple('GeneratorParameters', ('start', 'factor', 'divisor', 'multiple'))
 
-def generator(start, factor, divisor, limit=40000000):
+def generator(start, factor, divisor, multiple=1):
     value = start
-    for _ in range(limit):
+    while True:
         value = (value * factor) % divisor
-        yield value
-    raise StopIteration
+        if value % multiple == 0:
+            yield value
 
-def judgeGenerators(aStart, bStart, *, aFactor=16807, bFactor=48271, divisor=2147483647, genLimit=40000000):
-    matching = 0
-    aGen = generator(aStart, aFactor, divisor, limit=genLimit)
-    bGen = generator(bStart, bFactor, divisor, limit=genLimit)
-    #comparator = lambda (a,b): (a & 0xFFFF) == (b & 0xFFFF)
-    #return sum(map(comparator, zip(aGen, bGen)))
-    i=0
-    for a,b in zip(aGen, bGen):
-        #print("a={}\tb={}".format(a, b))
-        aMask, bMask = map(lambda x: x & 0xFFFF, (a,b))
-        if aMask == bMask:
-            matching += 1
+def comparator(pair):
+    a,b = pair
+    return (a & 0xFFFF) == (b & 0xFFFF)
 
-        if i % 1000 == 0:
-            print("{}/{}".format(i,genLimit), end="\r")
-        i+=1
-    return matching
+def judgeGenerators(aGenParam, bGenParam, limit=40000000):
+    #matching = 0
+    aGen = generator(*aGenParam)
+    bGen = generator(*bGenParam)
+    return sum(map(comparator, islice(zip(aGen, bGen), limit)))
+    #i=0
+    #for a,b in zip(aGen, bGen):
+    #    #print("a={}\tb={}".format(a, b))
+    #    aMask, bMask = map(lambda x: x & 0xFFFF, (a,b))
+    #    if aMask == bMask:
+    #        matching += 1
 
+    #    if i % 1000 == 0:
+    #        print("{}/{}".format(i,genLimit), end="\r")
+    #    i+=1
+    #return matching
+
+def parseArgs():
+    ap = argparse.ArgumentParser("AoC Day15: Generator Judgement")
+    ap.add_argument("a", type=int, help="Start value for generator A")
+    ap.add_argument("b", type=int, help="Start value for generator B")
+    ap.add_argument("--a-mult", type=int, default=1, help="Filter generator A to multiples of this value")
+    ap.add_argument("--b-mult", type=int, default=1, help="Filter generator B to multiples of this value")
+    ap.add_argument("-l", "--limit", type=int, default=40000000, help="Number of pairs to consider.")
+    return ap.parse_args()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("missing a start value")
-    else:
-        matching = judgeGenerators(int(sys.argv[1]), int(sys.argv[2]))
-        print("\n".format(matching))
+    args = parseArgs()
+
+    aFactor = 16807
+    bFactor = 48271
+    divisor = 2147483647
+
+    aGenParam = GenParam(args.a, aFactor, divisor, args.a_mult)
+    bGenParam = GenParam(args.b, bFactor, divisor, args.b_mult)
+    matching = judgeGenerators(aGenParam, bGenParam, limit=args.limit)
+    print(matching)
