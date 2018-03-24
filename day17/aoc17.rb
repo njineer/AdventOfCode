0a790ccd22e2fa3f2464a724805827bf960d92ba
zane <njineer@outlook.com>
2018-03-24 14:03:51 -0500

    day17.2 done

--- day17/aoc17.rb
+++ day17/aoc17.rb
@@ -2,19 +2,29 @@
 
 if __FILE__ == $0
     if ARGV.length < 1
-        puts "missing filename and step size"
+        puts "missing step size."
     else
         step = ARGV[0].to_i
         circle_buffer = [0]
         current = 0
 
         (1..2017).each do |i|
-            current = (current + step + 1) % circle_buffer.length
+            current = ((current + step) % i) + 1
             circle_buffer.insert(current, i)
         end
 
         index2017 = circle_buffer.find_index(2017)
-        puts "2017 @ #{index2017}"
+        #puts "2017 @ #{circle_buffer.find_index(2017)}"
         puts circle_buffer[(index2017 + 1) % circle_buffer.length]
+
+        current = 0
+        after_zero = 0
+        (1..50000000).each do |i|
+            current = ((current + step) % i) + 1
+            after_zero = i if current == 1 
+        end
+
+        puts after_zero
+
     end
 end
