aeb95fde3f8271ed0d515cc3d4c49958837e5180
zane <njineer@outlook.com>
2018-03-26 21:15:58 -0400

    day18: finish p1 improvement for p2

--- day18/aoc18.rb
+++ day18/aoc18.rb
@@ -1,74 +1,65 @@
 #!/usr/bin/env ruby
 
 class String
-    # convert to int if it's an int, else do nothing
-    # TODO: find a more ruby-ish way to notify if successful
-    def to_i_if_i
-        if /\A[-+]?\d+\z/ === self
-            [self.to_i, true]
-        else 
-            [self.to_s, false]
-        end
+    def is_i?
+        /\A[-+]?\d+\z/ === self
     end
 end
 
+
+def run(instructions, registers)
+    last_freq = nil
+    index = 0
+    get_reg_or_value = -> str { if str.is_i? then str.to_i else registers[str] end }
+
+    while true
+        instr = instructions[index]
+        cmd = instr[0].to_sym
+        arg1, arg2 = instr.drop(1)
+        
+        puts "#{cmd}, #{arg1}, #{arg2}"
+    
+        # process instructions
+        case cmd
+            when :snd 
+                last_freq = get_reg_or_value.call(arg1)
+            when :set 
+                registers[arg1] = get_reg_or_value.call(arg2)
+            when :add
+                registers[arg1] += get_reg_or_value.call(arg2)
+            when :mul
+                registers[arg1] *= get_reg_or_value.call(arg2)
+            when :mod
+                registers[arg1] %= get_reg_or_value.call(arg2)
+            when :rcv
+                break if registers[arg1] != 0
+            when :jgz
+            else 
+                puts "unknown instruction #{cmd}"
+        end
+    
+        # next instruction offset
+        if cmd === :jgz and get_reg_or_value.call(arg1) != 0
+            index += get_reg_or_value.call(arg2)
+        else
+            index += 1
+        end
+        if index < 0 or index >= instructions.length
+            break
+        end
+    end
+    last_freq
+end
 
 if __FILE__ == $0
     if ARGV.length < 1
         puts "missing input file"
     else 
         infile = ARGV[0]
-        registers = Hash.new
-        last_freq = nil
-        instructions = File.readlines(infile).map { |line| line.split(" ") }
+        registers = Hash.new(0)
+        instructions = File.readlines(infile).map { |line| line.split(" ") }.freeze
+        last_freq = run(instructions, registers)
 
-        # if there is an arg and the arg is a register
-        #   get register content
-        # else return arg
-        get_reg_or_value = lambda { |v|
-            if v
-                val, is_int = v.to_i_if_i
-                val = if !is_int then registers[val] else val end
-            end
-        }
-
-        index = 0
-        while true
-            instr, reg, arg = instructions[index]
-            # if there is an arg, get the int or the register content
-            value = get_reg_or_value.call(arg)
-            # make sure this register is initialized
-            registers[reg] = 0 if !registers.has_key? reg
-            # process instructions
-            case instr
-                when "snd" 
-                    last_freq = registers[reg]
-                when "set" 
-                    registers[reg] = value
-                when "add"
-                    registers[reg] += value
-                when "mul"
-                    registers[reg] *= value
-                when "mod"
-                    registers[reg] %= value
-                when "rcv"
-                    break if registers[reg] != 0
-                when "jgz"
-                else 
-                    puts "unknown instruction #{instr}"
-            end
-
-            # next instruction offset
-            cond = get_reg_or_value.call(reg)
-            if instr === "jgz" and cond != 0
-                index += value
-            else
-                index += 1
-            end
-            if index < 0 or index >= instructions.length
-                break
-            end
-        end
         puts "last freq = #{last_freq}"
     end
 end
