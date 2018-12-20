3e90f9ed3c192d8a85b13c846fa04871478477bb
zfrankli <zfrankli@cisco.com>
2018-12-20 16:35:08 -0700

    adapt 3.1 input for 3.2

--- 2018/day3/part2.cpp
+++ 2018/day3/part2.cpp
@@ -3,16 +3,47 @@
 #include <iterator>
 #include <string>
 #include <algorithm>
+#include <regex>
+#include <array>
+#include <map>
+#include <utility>
 
 using namespace std;
 
+struct InputParser {
+    const static regex num_re;
+    const static sregex_iterator re_itr_end;
+
+    array<int, 5> parse(const string& input) {
+        array<int, 5> claim;
+        auto re_itr_begin = sregex_iterator(input.begin(), input.end(), num_re);
+        transform(re_itr_begin, re_itr_end, claim.begin(), [](auto s){return stoi(s.str());});
+        return claim;
+    }
+};
+const regex InputParser::num_re("(-?\\d+)");
+const sregex_iterator InputParser::re_itr_end = sregex_iterator();
+
+
 int main (int argc, char** argv) {
 
-    // for each line
+    map<pair<int, int>, int> used_squares;
+    InputParser parser;
+
+    // extract numbers for each line into array
     for (string input; getline(cin, input);) {
-        if (any_of(input.begin(), input.end(), [](auto& chr){return !isspace(chr);}))
-        //
-    }
+        auto claim = parser.parse(input);
+        auto left   = claim[1];
+        auto top    = claim[2];
+        auto width  = claim[3];
+        auto height = claim[4];
+        for (int x=left; x < left+width; x++) {
+            for (int y=top; y < top+height; y++) {
+                used_squares[make_pair(x,y)] += 1;
+            }
+        }
+    } 
+    cout << count_if(used_squares.begin(), used_squares.end(), [](auto& sq){ return sq.second > 1; }) << endl;
 
     return 0;
 }
