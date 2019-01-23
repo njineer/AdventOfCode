35f97832803064aa23a8ef0e07f4b7219a6f0eeb
zfrankli <zfrankli@cisco.com>
2019-01-23 12:38:29 -0700

    7.2 in progress

--- 2018/day7/part2.cpp
+++ 2018/day7/part2.cpp
@@ -10,55 +10,111 @@
 
 using namespace std;
 
-template<typename T>
-void print_container(T&& c) {
-    cout << "{";
-    for (const auto& elem : c) {
-        cout << elem << ", ";
-    }
-    cout << "}" << endl;
+#define WORKER_COUNT 2 //5
+#define STEP_TIME 0 //60
+
+template <class T>
+void heap_push(vector<T>& v, const T& item) {
+    v.push_back(item);
+    push_heap(v.begin(), v.end());
 }
 
+template <class T>
+T heap_pop(vector<T>& v) {
+    pop_heap(v.begin(), v.end());
+    auto item = v.back();
+    v.pop_back();
+    return item;
+}
+
+
 int main (int argc, char** argv) {
+
+    // regex match single capital letters
     const regex letter_re("(\\b[A-Z]\\b)");
     const sregex_iterator re_itr_end = sregex_iterator();
 
+    // two directed graphs for tracking prerequisites and dependencies
     map<char, set<char>> prereqs;
     map<char, set<char>> deps;
+
+    // processing order for final answer
     vector<char> done;
 
+    // read all non-empty lines
     for (string input; getline(cin, input);) {
         if (all_of(input.begin(), input.end(), [](auto ch){ return isspace(ch); })) 
             break;
+
+        // first letter is prereq, second is dependency
         auto re_itr = sregex_iterator(input.begin(), input.end(), letter_re);
         assert(re_itr != re_itr_end);
         char prereq = (re_itr++)->str().front();
         char dep = (re_itr++)->str().front();
-        assert(re_itr == re_itr_end);
+        assert(re_itr == re_itr_end); // make sure input is relatively OK
         prereqs[dep].insert(prereq);
         deps[prereq].insert(dep);
     } 
 
+    // processing queue (alphabetical order)
     priority_queue<char, vector<char>, greater<char>> q;
-  
-    for (const auto& step : deps)
-        if (!prereqs.count(step.first))
+ 
+    // find steps with no prerequisites
+    for (const auto& step : deps) {
+        if (!prereqs.count(step.first)) {
             q.push(step.first);
-    
-    while(!q.empty()) {
-        auto cur = q.top();
-        q.pop();
-        for (auto& dep : deps[cur]) {
-            prereqs[dep].erase(cur);
-            if (prereqs[dep].empty()) {
-                q.push(dep);
-            }
         }
-        done.push_back(cur);
     }
 
-    for (const auto& step : done)
-        cout << step;
+    vector<pair<int, char>> wq;
+    wq.reserve(60);
+    while(!q.empty() || !wq.empty()) {
+
+        // while there are workers and work to be done
+        while(!q.empty() && wq.size() <= WORKER_COUNT) {
+            auto cur = q.top();
+            q.pop();
+            cout << "wq-ing " << cur << " with " << (STEP_TIME+cur-'A'+1) << endl;
+            // assign to worker (with time to completion)
+            heap_push(wq, make_pair(STEP_TIME + cur - 'A' + 1, cur)); // negative for min heap?
+        }
+
+        // clear out the done/zero-time-remaining steps
+        auto cur = heap_pop(wq);
+        while(cur.first <= 0) {
+            // for each dependency of this step
+            for (auto& dep : deps[cur.second]) {
+                // remove this step from its prereq list
+                prereqs[dep].erase(cur.second);
+                // if this was the last prereq, enqueue
+                if (prereqs[dep].empty()) {
+                    q.push(dep);
+                }
+            }
+            // track processing order
+            cout << "\talready done with " << cur.second << endl;
+            done.push_back(cur.second);
+            cur = heap_pop(wq);
+        }
+
+        // for each dependency of the nearest-to-completion step
+        for (auto& dep : deps[cur.second]) {
+            // remove this step from prereq list
+            prereqs[dep].erase(cur.second);
+            // if this was the last prereq, enqueue
+            if (prereqs[dep].empty()) {
+                q.push(dep);
+                cout << "\tq-ing " << dep << endl;
+            }
+        }
+        done.push_back(cur.second);
+        cout << "done with " << cur.second << endl;
+        // move time up to when the next step is done
+        for_each(wq.begin(), wq.end(), [&cur](auto& step){step.first -= cur.first;});
+        cout << "wq.size=" << wq.size() << ", q.size=" << q.size() << endl;
+    }
+
+    for_each(done.begin(), done.end(), [](const auto& chr){ cout << chr; });
     cout << endl;
 
     return 0;
