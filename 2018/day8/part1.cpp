b239d12ea35011da4aaab90056aa004ce1d8f119
zfrankli <zfrankli@cisco.com>
2019-01-25 12:11:57 -0700

    finish 8.1

--- 2018/day8/part1.cpp
+++ 2018/day8/part1.cpp
@@ -1,65 +1,31 @@
 #include <iostream>
 #include <string>
-#include <stack>
+#include <tuple>
 
 using namespace std;
 
-inline void dec(int& x) {
-    --x;
-    if (x < 0) x = 0;
+int parse(istream& is) {
+    int children, metadata, md_sum=0, md;
+
+    // read counts for this node
+    is >> children >> metadata;
+
+    // recursively accumulate metadata sum for each child
+    for (int i=0; i < children; i++) {
+        md_sum += parse(is);
+    } 
+
+    // accumulate metadata sum for this child
+    for (int i=0; i<metadata; i++) {
+        is >> md;
+        md_sum += md;
+    }
+    
+    // return total/recursive metadata sum for this node
+    return md_sum;
 }
 
 int main (int argc, char** argv) {
-
-    int pending_children = 0;
-    int md_sum = 0;
-    stack<int> pending_metadata;
-    int children, metadata, md;
-    int input;
-
-    // while there are 
-    while(cin >> input) {
-        cout << "[" << input << "] ";
-        children = input;
-        if (pending_children > 0) {
-            cout << "new child" << endl;
-            pending_children--;
-            cin >> metadata;
-            if (children == 0) {
-                cout << "\t0 children, consuming " << metadata << " metadata: {";
-                for (int i=0; i < metadata; i++){
-                    cin >> md;
-                    cout << md << ", ";
-                    md_sum += md;
-                }
-                cout << "}" << endl;
-            } else {
-                cout << "\t" << children << " children, " << metadata << " metadata" << endl;
-                pending_children += children;
-                pending_metadata.push(metadata);
-            }
-        } else if (!pending_metadata.empty()) {
-            md = input;
-            metadata = pending_metadata.top();
-            pending_metadata.pop();
-            cout << "consuming " << metadata << " metadata: {";
-            md_sum += md;
-            for (int i=1; i < metadata; i++){
-                cin >> md;
-                cout << md << ", ";
-                md_sum += md;
-            }
-            cout << "}" << endl;
-        } else {
-            cout << "new root" << endl;
-            children = input;
-            pending_children += children;
-            cin >> metadata;
-            pending_metadata.push(metadata);
-            cout << "\t" << children << " children, " << metadata << " metadata" << endl;
-        }
-    }
-
-    cout << endl << "metadata total: " << md_sum << endl;
+    cout << endl << "metadata total: " << parse(cin) << endl;
     return 0;
 }
