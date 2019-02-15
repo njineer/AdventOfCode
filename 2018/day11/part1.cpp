783c02517a99547b68ceef6c1acbf88ee5499a2f
zfrankli <zfrankli@cisco.com>
2019-02-15 13:11:23 -0700

    finish 11.1

--- 2018/day11/part1.cpp
+++ 2018/day11/part1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -32,6 +34,21 @@ int main (int argc, char** argv) {
             grid[z(x,y,RANGE)] = calc_fuel(x, y, serial);
         }
     }
+
+    map<pair<int, int>, int> grid_3x3;
+    for (int x=1; x < RANGE-2; x++) {
+        for (int y=1; y < RANGE-2; y++) {
+            grid_3x3[make_pair(x,y)] = 
+                grid[z(x, y,   RANGE)] + grid[z(x+1, y,   RANGE)]   + grid[z(x+2, y, RANGE)] +
+                grid[z(x, y+1, RANGE)] + grid[z(x+1, y+1, RANGE)] + grid[z(x+2, y+1, RANGE)] +
+                grid[z(x, y+2, RANGE)] + grid[z(x+1, y+2, RANGE)] + grid[z(x+2, y+2, RANGE)];
+        }
+    }
+
+    auto xy = max_element(grid_3x3.begin(), grid_3x3.end(), [](const auto& c1, const auto& c2){
+        return c1.second < c2.second; })->first;
+            
+    cout << "max fuel 3x3 @ " << xy.first << ", " << xy.second << endl;
 
     return 0;
 }
