de8acb77110e881ae20dc527fa00420d6432ed54
zfrankli <zfrankli@cisco.com>
2019-02-26 12:08:48 -0700

    update newday script

--- 2018/newday.sh
+++ 2018/newday.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
+    echo "Missing src"
+elif [ -z "$2" ]; then
     echo "Missing destination"
 else
-    cp -r day2 $1
-    cd $1
+    cp -r $1 $2
+    cd $2
     rm Makefile
     ln -s ../Makefile .
 fi
