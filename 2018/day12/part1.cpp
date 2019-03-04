0a9c9ffa6cd27b3dfc60325a5e3892d19752e230
zfrankli <zfrankli@cisco.com>
2019-03-04 15:10:55 -0700

    finish 12.1

--- 2018/day12/part1.cpp
+++ 2018/day12/part1.cpp
@@ -6,33 +6,92 @@
 #include <map>
 #include <set>
 #include <iterator>
+#include <numeric>
 
 using namespace std;
 
-enum class Neighbors : unsigned int {
-    LEFT2   = 0x10,
-    LEFT1   = 0x08,
-    CURRENT = 0x04,
-    RIGHT1  = 0x02,
-    RIGHT2  = 0x01
+#define GENERATIONS 20
+
+class Pots {
+    private:
+        set<int> plants;
+        set<unsigned int> rules;
+
+        inline bool get_plant(int i) {
+            auto itr = plants.find(i);
+            return itr != plants.end();
+        }
+
+        inline bool plant_next_gen(unsigned int pattern) {
+            return rules.find(pattern) != rules.end();
+        }
+
+        unsigned int get_pattern(int center) {
+            return get_plant(center-2) << 4 | 
+                   get_plant(center-1) << 3 | 
+                   get_plant(center)   << 2 | 
+                   get_plant(center+1) << 1 |
+                   get_plant(center+2);
+        }
+
+    public: 
+        Pots() { }
+
+        void init(const string& s) {
+            for (auto i=0; i < s.size(); i++) {
+                if (s[i] == '#') {
+                    plants.insert(i);
+                }
+            }
+        }
+
+        void add_rule(const string& pattern_str) {
+            unsigned int pattern = 0;
+            int i= pattern_str.size() - 1;
+            for(auto itr = pattern_str.begin(); itr != pattern_str.end(); itr++, i--) {
+                if (*itr == '#') {
+                    pattern |= 1 << i;
+                }
+            }
+            rules.insert(pattern);
+        }
+
+        void update() {
+            auto leftmost_plant = *plants.begin();
+            auto cur_pot = get_pattern(leftmost_plant - 3);
+            //cout << "starting @ " << leftmost_plant << endl;
+            for(int i=leftmost_plant-2; i < *plants.rbegin()+2; i++) {
+                cur_pot = (cur_pot << 1 | get_plant(i+2)) & 0x1f;
+                if (plant_next_gen(cur_pot)) {
+                    plants.insert(i);
+                }
+                else {
+                    plants.erase(i);
+                }
+            }
+        }
+
+        int plant_sum() {
+            return accumulate(plants.begin(), plants.end(), 0);
+        }
+
+        void display() {
+            for(int i=-3; i <= 35; i++) {
+                if (plants.count(i)) {
+                    cout << "#";
+                }
+                else {
+                    cout << ".";
+                }
+            }
+            cout << endl;
+        }
 };
 
-template <typename Iterator>
-unsigned int make_pattern(Iterator begin, Iterator end) {
-    unsigned int pattern = 0;
-    int i=distance(begin, end);
-    for(auto itr = begin; itr != end; itr++, i--) {
-        if (*itr == '#') {
-            pattern |= 1 << (i-1);
-        }
-    }
-    return pattern;
-}
 
 int main (int argc, char** argv) {
 
-    map<int, bool> pots;
-    set<unsigned int> rules;
+    Pots pots;
 
     const regex init_re("initial state:\\s+([#\\.]+)");
     const regex growth_re("([#\\.]+)\\s+=>\\s+([#\\.])");
@@ -42,15 +101,10 @@ int main (int argc, char** argv) {
     string input;
     getline(cin, input);
     if (regex_match(input, match, init_re)) {
-        auto initial = match.str(1);
-        for (auto i=0; i < initial.size(); i++) {
-            if (initial[i] == '#') {
-                pots[i] = true;
-            }
-        }
+        pots.init(match.str(1));
     } 
     else {
-        cout << "Expected initial state in first line, got '" << input << "'" << endl;
+        cout << "expected initial state in first line, got '" << input << "'" << endl;
         throw;
     }
 
@@ -62,17 +116,24 @@ int main (int argc, char** argv) {
 
         if (regex_match(input, match, growth_re)) {
             if (match.str(2) == "#") {
-                auto pattern_str = match.str(1);
-                rules.insert(make_pattern(pattern_str.begin(), pattern_str.end()));
+                pots.add_rule(match.str(1));
             }
         } 
         else {
-            cout << "Unexpected pattern: '" << input << "'" << endl;
+            cout << "unexpected pattern: '" << input << "'" << endl;
             throw;
         }
     }
-   
-    cout << hex; for (auto& r : rules) cout << r << endl;
-    
+
+    //cout << 0 << "\t";
+    // pots.display();
+    for (int i=1; i <= GENERATIONS; i++) {
+        // cout << i << "\t";
+        pots.update();         
+        // pots.display();
+    }
+
+    cout << pots.plant_sum() << endl;
+
     return 0;
 }
