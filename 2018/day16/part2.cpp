62878d90470d9e803d85a3c964f7bfe2e4b8f50e
zfrankli <zfrankli@cisco.com>
2019-03-26 15:36:37 -0600

    finish 16.2

--- 2018/day16/part2.cpp
+++ 2018/day16/part2.cpp
@@ -10,9 +10,16 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <cassert>
+#include <initializer_list>
+
+#define OPCODE_COUNT 16
 
 using namespace std;
 
+// all of this is kind of ugly. 
+// the use of array4i and array4i_ptr are from part1.
+// part 2 would likely be made better and more efficient by replacing them 
+//      (and rewriting the instruction functions), but it didn't seem worthwhile
 using array4i = array<int, 4>;
 using array4i_ptr = unique_ptr<array4i>;
 
@@ -25,19 +32,6 @@ struct Sample {
         instr = move(_instr);
         after = move(_after);
     }
-
-    string str() {
-        ostringstream ss;
-        int a,b,c,d;
-        tie(a,b,c,d) = *before;
-        ss << "before(" << a << ", " << b << ", " << c << ", " << d << "); ";
-        tie(a,b,c,d) = *instr;
-        ss << "instr(" << a << ", " << b << ", " << c << ", " << d << "); ";
-        tie(a,b,c,d) = *after;
-        ss << "after(" << a << ", " << b << ", " << c << ", " << d << "); ";
-        return ss.str();
-    }
-
 };
 
 class Instructions {
@@ -49,148 +43,154 @@ class Instructions {
             borr, bori,
             setr, seti,
             gtir, gtri, gtrr,
-            eqir, eqri, eqrr
+            eqir, eqri, eqrr,
+            unknown
         };
 
         static string name_str(const InstructionName& iname);
 
     private:
-        static array4i_ptr addr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr addi(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr mulr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr muli(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr banr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr bani(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr borr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr bori(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr setr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr seti(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr gtir(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr gtri(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr gtrr(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr eqir(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr eqri(const array4i_ptr& before, const array4i_ptr& instr);
-        static array4i_ptr eqrr(const array4i_ptr& before, const array4i_ptr& instr);
+        static array4i_ptr addr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr addi(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr mulr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr muli(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr banr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr bani(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr borr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr bori(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr setr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr seti(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr gtir(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr gtri(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr gtrr(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr eqir(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr eqri(const array4i_ptr& regs, const array4i_ptr& instr);
+        static array4i_ptr eqrr(const array4i_ptr& regs, const array4i_ptr& instr);
 
         static const 
             unordered_map<InstructionName, function<array4i_ptr(const array4i_ptr&, const array4i_ptr&)>> ops;
 
     public:
         static 
-            unordered_set<Instructions::InstructionName> compare(const unique_ptr<Sample>& sample);
+            unordered_set<InstructionName> compare(const unique_ptr<Sample>& sample);
+
+        static 
+            array4i_ptr exec_program(
+                const unordered_map<int, Instructions::InstructionName>& opcodes,
+                const vector<array4i_ptr>& instructions);
 };
 
 // addr (add register) stores into register C the result of adding register A and register B.
-array4i_ptr Instructions::addr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::addr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] + regs[b];
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] + new_regs[b];
+    return make_unique<array4i>(new_regs);
 }
 // addi (add immediate) stores into register C the result of adding register A and instrue B.
-array4i_ptr Instructions::addi(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::addi(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] + b;
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] + b;
+    return make_unique<array4i>(new_regs);
 }
 // mulr (multiply register) stores into register C the result of multiplying register A and register B.
-array4i_ptr Instructions::mulr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::mulr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] * regs[b];
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] * new_regs[b];
+    return make_unique<array4i>(new_regs);
 }
 // muli (multiply immediate) stores into register C the result of multiplying register A and instrue B.
-array4i_ptr Instructions::muli(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::muli(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] * b;
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] * b;
+    return make_unique<array4i>(new_regs);
 }
 // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
-array4i_ptr Instructions::banr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::banr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] & regs[b];
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] & new_regs[b];
+    return make_unique<array4i>(new_regs);
 }
 // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and instrue B.
-array4i_ptr Instructions::bani(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::bani(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] & b;
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] & b;
+    return make_unique<array4i>(new_regs);
 }
 // borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B.
-array4i_ptr Instructions::borr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::borr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] | regs[b];
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] | new_regs[b];
+    return make_unique<array4i>(new_regs);
 }
 // bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and instrue B.
-array4i_ptr Instructions::bori(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::bori(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a] | b;
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a] | b;
+    return make_unique<array4i>(new_regs);
 }
 // setr (set register) copies the contents of register A into register C. (Input B is ignored.)
-array4i_ptr Instructions::setr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::setr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = regs[a];
-    return make_unique<array4i>(regs);
+    new_regs[c] = new_regs[a];
+    return make_unique<array4i>(new_regs);
 }
 // seti (set immediate) stores instrue A into register C. (Input B is ignored.)
-array4i_ptr Instructions::seti(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::seti(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = a;
-    return make_unique<array4i>(regs);
+    new_regs[c] = a;
+    return make_unique<array4i>(new_regs);
 }
 // gtir (greater-than immediate/register) sets register C to 1 if instrue A is greater than register B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::gtir(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::gtir(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (a > regs[b]) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (a > new_regs[b]) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 // gtri (greater-than register/immediate) sets register C to 1 if register A is greater than instrue B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::gtri(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::gtri(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (regs[a] > b) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (new_regs[a] > b) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 // gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::gtrr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::gtrr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (regs[a] > regs[b]) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (new_regs[a] > new_regs[b]) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 // eqir (equal immediate/register) sets register C to 1 if instrue A is equal to register B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::eqir(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::eqir(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (a == regs[b]) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (a == new_regs[b]) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 // eqri (equal register/immediate) sets register C to 1 if register A is equal to instrue B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::eqri(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::eqri(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (regs[a] == b) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (new_regs[a] == b) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 // eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0.
-array4i_ptr Instructions::eqrr(const array4i_ptr& before, const array4i_ptr& instr) {
-    auto regs = *before;
+array4i_ptr Instructions::eqrr(const array4i_ptr& regs, const array4i_ptr& instr) {
+    auto new_regs = *regs;
     auto [_, a, b, c] = *instr;
-    regs[c] = (regs[a] == regs[b]) ? 1 : 0;
-    return make_unique<array4i>(regs);
+    new_regs[c] = (new_regs[a] == new_regs[b]) ? 1 : 0;
+    return make_unique<array4i>(new_regs);
 }
 
 const unordered_map<Instructions::InstructionName, function<array4i_ptr(const array4i_ptr&, const array4i_ptr&)>> 
@@ -248,6 +248,20 @@ string Instructions::name_str(const InstructionName& iname) {
     return name;
 };
 
+array4i_ptr Instructions::exec_program(
+    const unordered_map<int, Instructions::InstructionName>& opcodes,
+    const vector<array4i_ptr>& instructions)
+{
+    array4i_ptr regs = make_unique<array4i>();
+    std::for_each(regs->begin(), regs->end(), [](auto& x){ x = 0;});
+
+    for (auto& instr : instructions) {
+        auto op = opcodes.at(instr->at(0));
+        // cout << instr->at(0) << "\t" << name_str(op) << endl;
+        regs = move(ops.at(op)(regs, instr));
+    }
+    return regs;
+};
 
 array4i_ptr parse_line(const string& s, const regex& re) {
     smatch match;
@@ -320,28 +334,34 @@ int main (int argc, char** argv) {
         program.emplace_back(move(parse_line(input, num_re)));
     }
 
+    array<unordered_set<Instructions::InstructionName>, OPCODE_COUNT> possibilities;
     unordered_map<int, Instructions::InstructionName> opcodes;
 
-    // frequency of instructions
-    array<unordered_map<Instructions::InstructionName, int>, 16> match_counts;
-
+    // collect all of the possible instructions for each opcode using samples
     for (auto& sample : samples) {
+        auto sample_matches = Instructions::compare(sample);
         auto opcode = sample.get()->instr->at(0);
+        possibilities[opcode].insert(sample_matches.begin(), sample_matches.end());
+    }
 
-        auto potential_matches = Instructions::compare(sample);
-        for (auto& pm : potential_matches) { 
-            match_counts[opcode][pm]++;
+    // until we know all of the opcodes
+    while(opcodes.size() < OPCODE_COUNT) {
+        // find the first opcode with one remaining possibility
+        auto itr = find_if(possibilities.begin(), possibilities.end(), [](auto& p){
+            return p.size() == 1; });
+        if (itr == possibilities.end()) {
+            throw "No opcodes with a single matching sample";
+        }
+        auto instr = *(itr->begin());
+        opcodes[distance(possibilities.begin(), itr)] = instr;
+        for(auto& p : possibilities) {
+            p.erase(instr);
         }
     }
 
-    for (int i=0; i < match_counts.size(); i++) {
-        opcodes[i] = max_element(match_counts[i].begin(), match_counts[i].end(),
-            [](auto& p1, auto& p2) { return p1.second < p2.second; })->first;
-    }
+    array4i_ptr regs = Instructions::exec_program(opcodes, program);
 
-    for (auto& op: opcodes) {
-        cout << op.first << " " << Instructions::name_str(op.second) << endl;
-    }
+    cout << regs->at(0) << endl;
 
     return 0;
 }
