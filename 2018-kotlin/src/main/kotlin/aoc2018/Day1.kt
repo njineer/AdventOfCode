1a34e883567f1540b19a3f030c03b24a24a0af12
zfrankli <zfrankli@cisco.com>
2019-07-24 15:28:28 -0600

    1.2 done

--- 2018-kotlin/src/main/kotlin/aoc2018/Day1.kt
+++ 2018-kotlin/src/main/kotlin/aoc2018/Day1.kt
@@ -2,14 +2,30 @@ package aoc2018.days
 
 import aoc2018.foundation.parseInput
 
-fun day1_1(filename: String?) {
+fun getInput(filename: String?): MutableList<Int> {
     val inputs = mutableListOf<Int>()
     val appendInt: (String) -> Unit = { inputs.add(it.toInt()) }
     parseInput(filename, appendInt)
-    println(inputs.sum())
+    return inputs
+}
+
+fun day1_1(filename: String?) {
+    println(getInput(filename).sum())
 }
 
 fun day1_2(filename: String?) {
-    println("day1.1")
-
+    val seen = mutableSetOf<Int>()
+    val inputs = getInput(filename)
+    var sum = 0
+    while (true) {
+        inputs.forEach { i ->
+            sum += i
+            if (seen.contains(sum)) {
+                println(sum)
+                return
+            } else { 
+                seen.add(sum)
+            }
+        }
+    }
 }
